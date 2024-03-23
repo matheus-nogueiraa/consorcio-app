1d8a96905ecec680c63c56a4913bd04d876e5f33
Augusto França <augustofranca0701@gmail.com>
2024-03-23 00:21:32 -0300

    commit 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,16 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { GroupsComponent } from './components/groups/groups.component';
 import { ScreenDetailsGroupsComponent } from './screen-details-groups/screen-details-groups.component';
+import { BannerDetailsGroupsComponent } from "./components/banner-details-groups/banner-details-groups.component";
+import { FormDetailsGroupsComponent } from "./components/form-details-groups/form-details-groups.component";
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent, GroupsComponent,ScreenDetailsGroupsComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavbarComponent, HomeComponent, GroupsComponent, ScreenDetailsGroupsComponent, BannerDetailsGroupsComponent, FormDetailsGroupsComponent]
 })
 export class AppComponent {
   title = 'Seu Consórcio';
