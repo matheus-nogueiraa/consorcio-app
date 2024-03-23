339e86af64489fcd7df8404fd977494ad63ced25
Augusto França <augustofranca0701@gmail.com>
2024-03-22 23:08:05 -0300

    commit 01

---
+++ src/app/components/form-details-groups/form-details-groups.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-details-groups',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-details-groups.component.html',
+  styleUrl: './form-details-groups.component.css'
+})
+export class FormDetailsGroupsComponent {
+
+}
