ecd9f4debe73b68d83111ad515d5a9279434b072
Augusto França <augustofranca0701@gmail.com>
2024-03-24 16:46:43 -0300

    commit 9

--- src/app/screen-details-groups/screen-details-groups.component.ts
+++ src/app/screen-details-groups/screen-details-groups.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderDetailsGroupComponent } from "../components/header-details-group/header-details-group.component";
+import { BannerDetailsGroupsComponent } from "../components/banner-details-groups/banner-details-groups.component";
+import { FormDetailsGroupsComponent } from "../components/form-details-groups/form-details-groups.component";
 
 @Component({
-  selector: 'app-screen-details-groups',
-  standalone: true,
-  imports: [],
-  templateUrl: './screen-details-groups.component.html',
-  styleUrl: './screen-details-groups.component.css'
+    selector: 'app-screen-details-groups',
+    standalone: true,
+    templateUrl: './screen-details-groups.component.html',
+    styleUrl: './screen-details-groups.component.css',
+    imports: [HeaderDetailsGroupComponent, BannerDetailsGroupsComponent, FormDetailsGroupsComponent]
 })
 export class ScreenDetailsGroupsComponent {
 
