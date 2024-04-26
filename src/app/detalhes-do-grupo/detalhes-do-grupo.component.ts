443829baed50107e3bbe1eff69743941efbb9de3
Vinipcsl <viniciuspcsl@gmail.com>
2024-04-26 16:49:26 -0300

    Todos os fragmentos estão na tela

--- src/app/detalhes-do-grupo/detalhes-do-grupo.component.ts
+++ src/app/detalhes-do-grupo/detalhes-do-grupo.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { BannerDetailsGroupsComponent } from '../components/banner-details-groups/banner-details-groups.component';
-import { VisibilityService } from '../services/visibility.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-detalhes-do-grupo',
   standalone: true,
-  imports: [BannerDetailsGroupsComponent],
+  imports: [BannerDetailsGroupsComponent, MatInputModule, MatFormFieldModule, MatSlideToggleModule, MatButtonModule],
   templateUrl: './detalhes-do-grupo.component.html',
   styleUrl: './detalhes-do-grupo.component.css'
 })
 export class DetalhesDoGrupoComponent {
-  constructor(private visibilityService: VisibilityService) {
-    this.visibilityService.setShowComponent(false);
-  }
+
 }
