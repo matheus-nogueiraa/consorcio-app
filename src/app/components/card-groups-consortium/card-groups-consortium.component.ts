5c9661ec031747896cf001441714089fc4078ce9
Matheus Nogueira <matheus.vs.br@gmail.com>
2024-05-16 19:29:24 -0300

    fix cards

--- src/app/components/card-groups-consortium/card-groups-consortium.component.ts
+++ src/app/components/card-groups-consortium/card-groups-consortium.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-card-groups-consortium',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIcon],
   templateUrl: './card-groups-consortium.component.html',
   styleUrl: './card-groups-consortium.component.css'
 })
@@ -13,6 +14,19 @@ export class CardGroupsConsortiumComponent {
   @Input() totalCreditos?: string;
   @Input() valorParcelas?: string;
   @Input() totalParcelas?: string;
-  @Input() backgroundColor?: string;
   @Input() max_price?: string;
+  get backgroundColor(): string {
+    const price = Number(this.max_price);
+    if (price <= 500) {
+      return '#61787b';
+    } else if (price <= 1000) {
+      return '#61787b';
+    } else if (price <= 5000) {
+      return '#bf5c45';
+    } else if (price <= 10000) {
+      return '#2b5278';
+    } else {
+      return '#defaultColor'; // substitua 'defaultColor' pela cor que você deseja usar como padrão
+    }
+  }
 }
